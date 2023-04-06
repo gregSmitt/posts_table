af08c2f2356646ddf22887010f9be3d027c1a80d
gregSmitt <89312058+gregSmitt@users.noreply.github.com>
2023-04-06 21:09:23 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -71,22 +71,22 @@
 			this.model = model;
 			this.view = view;
 			this.model.requestData().then((() => {
-				this.updateView();
+				this._updateView();
 			}));
 		}
 		filterPostsByString = str => {
 			this.model.filterPosts(str);
-			this.updateView();
+			this._updateView();
 		};
 		orderPostsBy = (flag, isASC) => {
 			this.model.setSortFlag(flag, isASC);
-			this.updateView();
+			this._updateView();
 		};
 		getSortFlags = () => this.model.getSortFlags();
 		getPosts = () => {
 			if (this.model.getPosts().length) return this.model.getPosts().filter((post => post.isCorrect)); else return null;
 		};
-		updateView() {
+		_updateView() {
 			this.view.processData({
 				sortFlags: this.getSortFlags(),
 				posts: this.getPosts(),
@@ -103,13 +103,26 @@
 				title: "title",
 				body: "body"
 			};
+			this._initialElements()
+			this._render()
+		}
+		processData({ sortFlags, posts, orderPostsBy, filterPostsByString }) {
+			this._clearTrash();
+			this.orderPostsBy = orderPostsBy;
+			this.filterPostsByString = filterPostsByString;
+			this.sortFlags = sortFlags;
+			for (const key in this.postKeys) if (sortFlags[key].isActive) this[key].innerHTML = key + (sortFlags[key].isASC ? "↑" : "↓");
+			this._handleEvents();
+			this._render(posts);
+		}
+		_initialElements() {
 			this.root = document.getElementById("root");
 			this.input = document.createElement("input");
-			this.root.prepend(this.input);
 			this.input.className = "input";
 			this.input.placeholder = "Введите строку";
 			this.table = document.createElement("div");
 			this.table.className = "table";
+
 			this.id = document.createElement("div");
 			this.userId = document.createElement("div");
 			this.title = document.createElement("div");
@@ -118,60 +131,51 @@
 				this[key].className = `table__item ${key}`;
 				this[key].innerHTML = key;
 			}
-			this.render();
+			this.root.prepend(this.input)
 		}
-		processData({ sortFlags, posts, orderPostsBy, filterPostsByString }) {
-			this.clearTrash();
-			this.orderPostsBy = orderPostsBy;
-			this.filterPostsByString = filterPostsByString;
-			this.sortFlags = sortFlags;
-			for (const key in this.postKeys) if (sortFlags[key].isActive) this[key].innerHTML = key + (sortFlags[key].isASC ? "↑" : "↓");
-			this.handleEvents();
-			this.render(posts);
+		_handleEvents() {
+			this.id.addEventListener("click", this._handleIdClick);
+			this.userId.addEventListener("click", this._handleUserIdClick);
+			this.title.addEventListener("click", this._handleTitleClick);
+			this.body.addEventListener("click", this._handleBodyClick);
+			this.input.addEventListener("input", this._handleInput);
 		}
-		handleEvents() {
-			this.id.addEventListener("click", this.handleIdClick);
-			this.userId.addEventListener("click", this.handleUserIdClick);
-			this.title.addEventListener("click", this.handleTitleClick);
-			this.body.addEventListener("click", this.handleBodyClick);
-			this.input.addEventListener("input", this.handleInput);
+		_clearTrash() {
+			this.id.removeEventListener("click", this._handleIdClick);
+			this.userId.removeEventListener("click", this._handleUserIdClick);
+			this.title.removeEventListener("click", this._handleTitleClick);
+			this.body.removeEventListener("click", this._handleBodyClick);
+			this.input.removeEventListener("input", this._handleInput);
 		}
-		clearTrash() {
-			this.id.removeEventListener("click", this.handleIdClick);
-			this.userId.removeEventListener("click", this.handleUserIdClick);
-			this.title.removeEventListener("click", this.handleTitleClick);
-			this.body.removeEventListener("click", this.handleBodyClick);
-			this.input.removeEventListener("input", this.handleInput);
-		}
-		handleIdClick = () => {
+		_handleIdClick = () => {
 			const ID = this.postKeys.id;
 			this.orderPostsBy(ID, !this.sortFlags[ID].isASC);
 		};
-		handleInput = () => {
+		_handleInput = () => {
 			this.filterPostsByString(this.input.value);
 		};
-		handleUserIdClick = () => {
+		_handleUserIdClick = () => {
 			const USER_ID = this.postKeys.userId;
 			this.orderPostsBy(USER_ID, !this.sortFlags[USER_ID].isASC);
 		};
-		handleTitleClick = () => {
+		_handleTitleClick = () => {
 			const TITLE = this.postKeys.title;
 			this.orderPostsBy(TITLE, !this.sortFlags[TITLE].isASC);
 		};
-		handleBodyClick = () => {
+		_handleBodyClick = () => {
 			const BODY = this.postKeys.body;
 			this.orderPostsBy(BODY, !this.sortFlags[BODY].isASC);
 		};
-		createLine(post) {
+		_createLine(post) {
 			return `\n\t\t\t<div class="table__item">${post.id}</div>\n\t\t\t<div class="table__item">${post.userId}</div>\n\t\t\t<div class="table__item">${post.title}</div>\n\t\t\t<div class="table__item">${post.body}</div>\n\t\t`;
 		}
-		render(posts) {
+		_render(posts) {
 			this.root.append(this.table);
 			if (this.table.innerHTML) this.table.innerHTML = "";
 			if (posts) {
 				for (const key in this.postKeys) this.table.append(this[key]);
 				posts.forEach((post => {
-					this.table.insertAdjacentHTML("beforeend", this.createLine(post));
+					this.table.insertAdjacentHTML("beforeend", this._createLine(post));
 				}));
 			} else this.table.insertAdjacentHTML("beforeend", "<div>Lodaing...</div>");
 		}
